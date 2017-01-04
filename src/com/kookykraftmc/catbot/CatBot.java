ff71e7c75dff1c7efe50a5f7816f633643f52672
Kraise <fronk.fn@gmail.com>
2017-01-04 16:50:57 +0000

    Initial Commit
    - Currently only filters chat

---
+++ src/com/kookykraftmc/catbot/CatBot.java
@@ -0,0 +1,109 @@
+package com.kookykraftmc.catbot;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.spongepowered.api.Game;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.args.GenericArguments;
+import org.spongepowered.api.command.source.ConsoleSource;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.config.DefaultConfig;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.util.Color;
+
+import com.google.inject.Inject;
+import com.kookykraftmc.catbot.listeners.ChatFilter;
+
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+
+@Plugin(id="catbot", name="CatBot", version="1.0",
+        description="KKMC's adorable chat plugin.")
+public class CatBot
+{
+    final public static String CHAT_PREFIX = Color.DARK_MAGENTA + "[" + Color.MAGENTA + "CatBot" + Color.DARK_MAGENTA + "]" + Color.WHITE;
+    final public static String CONSOLE_PREFIX = "[CatBot]";
+    
+    @Inject
+    public Game game;
+    
+    @Inject
+    public static Logger log;
+    
+    @DefaultConfig(sharedRoot = false)
+    @Inject
+    private ConfigurationLoader<CommentedConfigurationNode> configLoader;
+    
+    @Listener
+    public void onServerStart(GameStartedServerEvent e) throws IOException
+    {
+        //ToDo Load config
+        /**
+        try
+        {
+            CommentedConfigurationNode node = configLoader.load();
+            config = Config.MAPPER.bindToNew().populate(node);
+            Config.MAPPER.bind(config).serialize(node);
+            configLoader.save(node);
+        } catch (ObjectMappingException ex) {
+            log.error("Couldn't populate Config!", ex);
+        }
+        **/
+        registerCommands();
+        Sponge.getEventManager().registerListeners(this, new ChatFilter(this));
+
+    }
+    
+    public static void sendMsg(CommandSource sender, String msg)
+    {
+        String prefix;
+        prefix = (sender instanceof ConsoleSource)?CONSOLE_PREFIX:CHAT_PREFIX;
+        Text txt = Text.builder(prefix + msg).toText();
+        sender.sendMessage(txt);
+    }
+    
+    private void registerCommands()
+    {
+        CommandSpec commandReload = CommandSpec.builder()
+                .description(Text.of("Reload CatBot's config."))
+                .permission("catbot.command.reload")
+                .arguments(GenericArguments.none())
+                .executor(new CommandExecutor() {
+                    @Override
+                    public CommandResult execute(CommandSource sender, CommandContext args) throws CommandException
+                    {
+                        return CommandResult.success();
+                        //ToDo reload config
+                    }
+                })
+                .build();
+        Sponge.getCommandManager().register(this, commandReload, "catbotreload");
+        
+        CommandSpec commandPet = CommandSpec.builder()
+                .description(Text.of("Meow."))
+                //.permission() everyone can use it
+                .arguments(GenericArguments.none())
+                .executor(new CommandExecutor() {
+                    @Override
+                    public CommandResult execute(CommandSource sender, CommandContext args) throws CommandException
+                    {
+                        sendMsg(sender, "Meow :3");
+                        return CommandResult.success();
+                    }
+                })
+                .build();
+        Sponge.getCommandManager().register(this, commandPet, "catbotpet");
+        
+        
+    }
+    
+}
---
+++ src/com/kookykraftmc/catbot/listeners/ChatFilter.java
@@ -0,0 +1,82 @@
+package com.kookykraftmc.catbot.listeners;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.message.MessageChannelEvent;
+import org.spongepowered.api.text.Text;
+
+import com.kookykraftmc.catbot.CatBot;
+
+public class ChatFilter
+{
+    final static public Random rdm = new Random();
+    final static Logger log = CatBot.log;
+    static public CatBot plugin;
+    static public HashSet<String> badWords = new HashSet<String>(Arrays.asList("\\bl(a|@)g+(y|ing|s)?\\b","dick","fuck","\\b(bull)?shitt?(ing|y)?\\b","penis","vagina","fag","\\brape","slut","hitler","\\b(jack)?ass(holes?|lick|wipe)?\\b","arse(hole)?","bitch","whore","nigg(er|a)","bastard","bea?stiality","negro","retard","\\bcum\\b","cunt","dildo","bollocks?","\\bwank","jizz","piss"));
+    static public String denyMsg;
+    static public List<String> replacingWords = Arrays.asList("meow","hiss","purr");
+    
+    public ChatFilter(CatBot cb)
+    {
+        plugin = cb;
+        loadCfg();
+    }
+
+    @Listener
+    public void onChat(MessageChannelEvent e)
+    {
+        //ToDon't filter if sender has permission to bypass
+        boolean isFiltered = false;
+        if(!(e instanceof MessageChannelEvent.Chat)) return;
+        Text msg = e.getMessage();
+        List<Text> msgs = e.getMessage().getChildren();
+        Text newMessage = Text.EMPTY;
+        for(Text msgPart:msgs)
+        {
+            String msgPlain = msgPart.toPlain();
+            String filteredMsg = filter(msgPlain);
+            if(!msgPlain.equals(filteredMsg))
+            {
+                isFiltered = true;
+                msgPart = Text.builder(filteredMsg).toText();
+            }
+            newMessage = Text.builder().append(newMessage, msgPart).build();
+        }
+        if(isFiltered)
+        {
+            e.setMessage(newMessage);
+            log.info("Original message: " + msg);
+        //ToDo: Send player denyMsg when their message is filtered
+        }
+    }
+    
+    /**
+     * Filters message to replace bad words
+     * 
+     * @param message Message to filter
+     * @return Filtered message
+     */
+    public String filter(String message)
+    {
+            for (String bad:badWords)
+            {
+                if(message.toLowerCase().matches("(?iu).*" + bad + ".*"))
+                {
+                    String replacingWord = replacingWords.get(rdm.nextInt(replacingWords.size()));
+                    message = message.replaceAll("(?iu)" + bad,replacingWord);
+                }
+            }
+            return message;
+    }
+    
+    public void loadCfg()
+    {
+        //ToDo: Make it load from config
+    }
+    
+}
